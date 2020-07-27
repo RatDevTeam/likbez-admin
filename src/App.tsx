076b084c4bc4e4dd28a650fb3729fe9ddfce5b6a
PutilovDmitriy <dmitrii.98.putilov@gmail.com>
2020-07-27 19:42:36 +0500

    start project

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './styles.scss';
+
+const App: React.FC = () => {
+	return <h1>Hello World</h1>;
+};
+
+export default App;
---
+++ src/redux/store/index.ts
@@ -0,0 +1,12 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk, { ThunkMiddleware } from 'redux-thunk';
+import rootReducer from '../reducers';
+import { AppActionType } from '../actions/action.types';
+
+export type AppState = ReturnType<typeof rootReducer>;
+
+export const Store = createStore(
+  rootReducer,
+  // eslint-disable-next-line prettier/prettier
+  applyMiddleware(thunk as ThunkMiddleware<AppState, AppActionType>),
+);
