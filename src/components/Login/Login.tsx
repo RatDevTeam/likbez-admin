7bc796c4cf41634bcc0ea538988610ac9607efc9
Putilov Dmitriy <dmitrii.98.putilov@gmail.com>
2020-09-30 14:32:08 +0500

    auth-user добавил пробную авторизацию

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const Auth: React.FC = () => {
+	const [login, setLogin] = useState();
+	const [password, setPassword] = useState();
+	const handleLogin = () => {
+		axios
+			.post(
+				'/auth/login',
+				{ email: login, password },
+				{ withCredentials: true }
+			)
+			.then((res) => console.log(res))
+			.catch((err) => console.log(err));
+	};
 	return (
 		<div>
 			<div>
 				<h3>Войти</h3>
-				<input type="text" />
-				<input type="password" />
+				<input
+					type="text"
+					value={login}
+					onChange={(event) => setLogin(event.target.value)}
+				/>
+				<input
+					type="password"
+					value={password}
+					onChange={(event) => setPassword(event.target.value)}
+				/>
+				<button onClick={() => handleLogin()}>LOGIn</button>
 			</div>
 		</div>
 	);
