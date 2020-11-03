6df6640f92ca74d3ae719a31a32ae23c9779ab40
PutilovDmitriy <44982167+PutilovDmitriy@users.noreply.github.com>
2020-11-03 23:33:51 +0500

    Revert "Feature/courses page"

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,8 @@
-import React, { lazy, Suspense } from 'react';
-import { useRouteNode } from 'react-router5';
+import React from 'react';
 import './styles.scss';
-import Login from './components/Login/Login';
-import SideBar from './components/SideBar/SideBar';
-
-const CoursesPage = lazy(() => import('./containers/CoursesPage'));
-const UsersPage = lazy(() => import('./containers/UsersContainer'));
 
 const App: React.FC = () => {
-	const { route } = useRouteNode('');
-	const topRouteName = route.name.split('.')[0];
-
-	const routing = (): React.ReactElement => {
-		if (topRouteName === 'login') {
-			return <Login />;
-		}
-		if (topRouteName === 'courses') {
-			return <CoursesPage />;
-		}
-		if (topRouteName === 'users') {
-			return <UsersPage />;
-		}
-		return <h1>Пусто</h1>;
-	};
-
-	return (
-		<>
-			<SideBar />
-			<div className="main-block">
-				<Suspense fallback={<div>Загрузка...</div>}>{routing()}</Suspense>
-			</div>
-		</>
-	);
+	return <h1>Hello World</h1>;
 };
 
 export default App;
